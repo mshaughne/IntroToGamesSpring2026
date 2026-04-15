4b29c7ce7e05db1d58fc7ecdba95b742634cd601
Michael <187816372+mshaughne@users.noreply.github.com>
2026-04-15 09:35:59 -0400

    Audio lecture

--- Assets/Scripts/BallAnimation.cs
+++ Assets/Scripts/BallAnimation.cs
@@ -6,10 +6,15 @@ public class BallAnimation : MonoBehaviour
 {
 	[SerializeField] Animator animator;
 
+	[Header("Audio")]
+	[SerializeField] AudioSource src;
+	[SerializeField] AudioClip jumpSound, pressSound, releaseSound;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		animator = GetComponent<Animator>();
+		src = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -17,11 +22,25 @@ public class BallAnimation : MonoBehaviour
 	{
 		if(Input.GetButtonDown("Jump"))
 		{
+			//src.clip = pressSound;
+			//src.Play();
+			AudioSource.PlayClipAtPoint(pressSound, transform.position, 1f);
+
 			animator.SetBool("isSpaceBarHeld", true);
 		}
 		else if (Input.GetButtonUp("Jump"))
 		{
-			animator.SetBool("isSpaceBarHeld", false);
+            //src.clip = releaseSound;
+            //src.Play();
+            AudioSource.PlayClipAtPoint(releaseSound, transform.position, 1f);
+
+            animator.SetBool("isSpaceBarHeld", false);
 		}
+	}
+
+	public void PlaySound()
+	{
+		src.clip = jumpSound;
+		src.Play();
 	}
 }
